c29c1ea3cf315473f6f821885d52917e89c444b1
Daniel <danielpellegrini87@gmail.com>
2020-12-15 16:04:23 +0100

    added function on dice game

--- js/script.js
+++ js/script.js
@@ -8,26 +8,23 @@ if (email === 'pinco.pallino@email.com') {
   alert('Unknown user. \nPlease try again');
 }
 
+var clickElement = document.getElementById('button-element');
 
+clickElement.addEventListener('click',
+  function() {
+    var user = Math.floor(Math.random() * 6) + 1;
+    document.getElementById('dice').innerHTML = user;
 
+    // Computer
+    var computer = Math.floor(Math.random() * 6) + 1;
+    document.getElementById('dice').innerHTML = computer;
 
-
-// Gioco Dadi
-
-// User
-var user =  Math.floor(Math.random() * 6) + 1;
-document.getElementById('dice').innerHTML = user;
-
-// Computer
-var computer = Math.floor(Math.random() * 6) + 1;
-document.getElementById('dice').innerHTML = computer;
-
-if (user > computer) {
-  document.getElementById('dice').innerHTML = 'Ha vinto ' + email + '!';
-}
-else if (user === computer) {
-  document.getElementById('dice').innerHTML = 'Pareggio';
-}
-else {
-  document.getElementById('dice').innerHTML = 'Ha vinto il computer';
-}
+    if (user > computer) {
+      document.getElementById('dice').innerHTML = 'Ha vinto ' + email + '!';
+    } else if (user === computer) {
+      document.getElementById('dice').innerHTML = 'Pareggio';
+    } else {
+      document.getElementById('dice').innerHTML = 'Ha vinto il computer';
+    }
+  }
+);
